fd8842d9288bf9155246b68fe9c4706cfe06e871
Robolacion <agusrobol@gmail.com>
2024-08-22 20:57:08 -0300

    wiriwiri

--- clases/15-08-2024/algo_lib.cpp
+++ clases/15-08-2024/algo_lib.cpp
@@ -10,10 +10,11 @@ Din_Vmk::Din_Vmk(int n)
 {
    v=new int[n];
 }
-/* destructor
-::~()
-{
-}*/
+Din_Vmk::Din_Vmk(Din_Vmk& aux){
+    aux.size=size;
+    aux.v=new int[size];
+
+}
 // Desarrollo
 void Din_Vmk::set_v(int pos, int val)
 {
@@ -34,4 +35,36 @@ int Din_Vmk::get_v(int pos)
 int& Din_Vmk::operator[](int pos){
     if(size>pos && pos<size)
     return v[pos]; 
+}
+
+void Din_Vmk::resize(int new_size){
+  int i=0,n[size];
+    for(i=0;i<size;i++)
+    {
+     n[i]=v[i];
+    }
+    v=new int[new_size];
+    for(i=0;i<size;i++)
+    {
+     v[i]=n[i];
+    }
+    size=new_size;
+}
+void Din_Vmk:: concat(Din_Vmk v2){
+    int new_size,n[size],i;
+    new_size=size+v2.size;
+    
+    for(i=0;i<size;i++)
+    {
+     n[i]=v[i];
+    }
+    v=new int [new_size];
+    for (i = 0; i < new_size; i++)
+    {
+        if (i<size)
+            v[i]=n[i];
+        else
+            v[i]=v2.v[i];    
+    }
+    size=new_size;
 }
--- clases/15-08-2024/algo_lib.h
+++ clases/15-08-2024/algo_lib.h
@@ -8,9 +8,14 @@ private:
 public:
 Din_Vmk();
 Din_Vmk(int n);
+Din_Vmk(Din_Vmk& aux);
+~Din_Vmk(){delete[]v;}
 void set_v(int pos, int val);
 int get_v(int pos);
 //int operator[](int n);
 int& operator[](int pos);
 int GetSize(){return size;}
+void resize(int new_size);
+void concat(Din_Vmk v2);
+
 };
